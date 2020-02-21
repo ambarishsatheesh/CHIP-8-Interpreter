2e67a8469f1d42c1e68dfef05e103db496b42c6d
Ambarish Satheesh <43656382+ambarishsatheesh@users.noreply.github.com>
2020-02-21 17:47:15 +0000

    Update Chip8.h

--- Chip8.h
+++ Chip8.h
@@ -1,39 +1,62 @@
 #pragma once
 #include "defines.h"
 
-#include <cstdint>
-#include <chrono>
 #include <random>
+
+const unsigned int KEY_COUNT = 16;
+const unsigned int MEMORY_MAX = 4096;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVELS = 16;
+const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int VIDEO_WIDTH = 64;
+
+const unsigned int START_ADDRESS = 0x200;
+const unsigned int FONT_SIZE = 80;
+const unsigned int FONT_START_ADDRESS = 0x50;
 
 class Chip8
 {
 public:
-	uint8_t registers[16]{};	//dedicated CPU storage
-	uint8_t memory[4096]{};		//general memory
+	Chip8();
+	void LoadROM(char const* filename);
+	void Cycle();
+
+	//public accessed by main.cpp
+	uint32_t video[VIDEO_WIDTH * VIDEO_HEIGHT]{};	//64 px * 32 px display memory buffer
+	bool keypad[KEY_COUNT]{};
+	//game speed control
+	float speed = 0;
+	float* pGameSpeed = &speed;
+
+private:
+	uint8_t registers[REGISTER_COUNT]{};	//dedicated CPU storage
+	uint8_t memory[MEMORY_MAX]{};		//general memory
 	uint16_t index{};	//Index Register - stores memory addresses for use in operations
 	uint16_t pc{};	//Program Counter - holds address of next instruction
-	uint16_t stack[16]{};	//keep track of execution order (call stack)
+	uint16_t stack[STACK_LEVELS]{};	//keep track of execution order (call stack)
 	uint8_t sp{};	//Stack Pointer (to index of stack array) - keep track of stack level where most recent value was placed
 	uint8_t delayTimer{};
 	uint8_t soundTimer{};
-	uint8_t keypad[16]{};
-	uint32_t video[2048]{};	//64 px * 32 px display memory buffer
 	uint16_t opcode;
 
+	std::vector<uint8_t> keyList;
 
 	std::default_random_engine randGen;
-	std::uniform_int_distribution<uint8_t> randByte; //uint8_t not valid template parameter?
+	std::uniform_int_distribution<int> randByte; //uint8_t not valid template parameter?
 
+	void Table0();
+	void Table8();
+	void TableE();
+	void TableF();
 
-	Chip8();
-	void LoadROM(char const* filename);
-
-private:
+	//dummmy void function in case invalid opcode is called
+	//this will be default function called if proper func pointer is not set
+	void OP_NULL();
 
 //----------------------------------
 //			Opcode declarations
 //----------------------------------
-
+												
 	//CLS
 	void OP_00E0();
 	//RET
@@ -66,13 +89,13 @@ private:
 	void OP_8xy5();	
 	//SHR Vx {, Vy}
 	void OP_8xy6();
-	//alternate SHR Vx { , Vy }
+	//Alternate SHR Vx { , Vy }
 	void OP_8xy6_alt();
 	//SUBN Vx, Vy
 	void OP_8xy7();
 	//SHL Vx {, Vy}
 	void OP_8xyE();
-	//alternate SHL Vx {, Vy}
+	//Alternate SHL Vx {, Vy}
 	void OP_8xyE_alt();
 	//SNE Vx, Vy
 	void OP_9xy0();
@@ -82,7 +105,47 @@ private:
 	void OP_Bnnn();
 	//RND Vx, byte
 	void OP_Cxkk();
+	//DRW Vx, Vy, nibble
+	void OP_Dxyn();
+	//SKP Vx
+	void OP_Ex9E();
+	//SKNP Vx
+	void OP_ExA1();
+	//LD Vx, DT
+	void OP_Fx07();
+	//LD Vx, K
+	void OP_Fx0A();
+	//LD DT, Vx
+	void OP_Fx15();
+	//LD ST, Vx
+	void OP_Fx18();
+	//ADD I, Vx
+	void OP_Fx1E();
+	//LD F, Vx
+	void OP_Fx29();
+	//LD B, Vx
+	void OP_Fx33();
+	//LD [I], Vx
+	void OP_Fx55();
+	//Alternate LD [I], Vx
+	void OP_Fx55_alt();
+	//LD Vx, [I]
+	void OP_Fx65();
+	//Alternate LD [I], Vx
+	void OP_Fx65_alt();
 
-	//8xy6, 8xye, Fx55, and Fx65 - don't use cowgod as reference
+	//define pointer-to-function type
+	using Chip8Func = void (Chip8::*)();
+	typedef void (Chip8::*Chip8Func)();
+	//index up to 0xF + 1 (16)
+	Chip8Func table[0xF + 1]{ &Chip8::OP_NULL };
+	//index up to 0xE + 1 (15)
+	Chip8Func table0[0xE + 1]{ &Chip8::OP_NULL };
+	//index up to 0xE + 1 (15)
+	Chip8Func table8[0xE + 1]{ &Chip8::OP_NULL };
+	//index up to 0xE + 1 (15)
+	Chip8Func tableE[0xE + 1]{ &Chip8::OP_NULL };
+	//index up to 0x65 + 1 (102)
+	Chip8Func tableF[0x65 + 1]{ &Chip8::OP_NULL };
 };
 
